36b0d7c21cb143363d7b2e33e1c90075c747ff6d
Jackson Kang <goodday1478@naver.com>
2018-10-20 17:45:21 +0900

    kNN completed

--- HW03_kNN/main.py
+++ HW03_kNN/main.py
@@ -1,6 +1,10 @@
 from modules import data_loader
 from modules import kNN
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+import numpy as np
+
 import argparse
 
 
@@ -20,11 +24,9 @@ def take_argument():
 
 if __name__ == "__main__":
 
-
-	train_data_number_list = [1000, 2000]
-	#[1000, 2000, 5000, 10000, 25000, 50000]
-	test_data_number_list = [100]
-	#[100, 200, 500, 1000, 2000, 5000, 10000]
+	
+	train_data_number_list = [1000, 2000, 5000, 10000, 25000, 50000]
+	test_data_number_list = [100, 200, 500, 1000, 2000, 5000, 10000]
 
 	k_list = [1, 5, 10]
 	eigenspace_dimension_list = [2, 5, 10]
@@ -32,7 +34,7 @@ if __name__ == "__main__":
 
 	images = data_loader.load_data('mnist.pkl.gz')
 	knn = kNN.knn_init(data=images)
-
+	'''
 	for train_number in train_data_number_list:
 		for test_number in test_data_number_list:
 			for k in k_list:
@@ -43,6 +45,20 @@ if __name__ == "__main__":
 			for k in k_list:
 				for eigenspace_dimension in eigenspace_dimension_list:
 					knn.run(k=k, train_number = train_number, test_number = test_number, eigenspace_dimension = eigenspace_dimension)
+	'''
+
+
+
+	clf = RandomForestClassifier(n_estimators = 200, max_depth = 500)
+	clf.fit(images[0], images[1])
+
+	#print(clf.feature_importances_)
+
+	acc = np.mean(np.equal(clf.predict(images[2]), images[3]))
+	print("Accuracy is ", acc)
+
+	
 
 
 	print("finish!")
+
