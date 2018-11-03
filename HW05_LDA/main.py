94707f7a729586a074de22237d733d58a6fa0f82
Jackson Kang <goodday1478@naver.com>
2018-11-04 01:37:11 +0900

    LDA bug fixed

--- HW05_LDA/main.py
+++ HW05_LDA/main.py
@@ -1,5 +1,17 @@
+'''
+
+	Machine Learning Homework #5
+
+	- Implement and apply LDA to MNIST to find 2, 3, 5, 9 dim spaces.
+
+	- Compare them to the corresponding eigenspaces in classification accuracy (with kNN and random forest)
+	
+	- Written by 21300008 Kang Min-Su
+
+'''
+
 from modules import data_loader
-from modules import kNN
+from modules import modules
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
@@ -8,24 +20,17 @@ import numpy as np
 import argparse
 
 
-# Machine Learning HW#3 
-
-#  - implement k-Nearest Neighbor(kNN)  and use Random Forest
-#  - implemented by Jackson Kang, 2018. 10. 05.
-
-
-
 if __name__ == "__main__":	
 
 	images = data_loader.load_data('mnist.pkl.gz')
 
-
-	k_list = [3]
+	k_list = [1, 3, 5, 10]
 	eigenspace_list = [2, 3, 5, 9]
-	train_number_list = [1000, 2000, 5000, 10000, 25000, 50000]
-	test_number_list = [100, 500, 1000, 5000, 10000]
+	train_number_list = [50000]
+	test_number_list = [10000]
+	mode_list = ["PCA", "LDA"]
 
-
+	'''
 	for k in k_list:
 		for dim in eigenspace_list:
 			for train_number in train_number_list:
@@ -34,8 +39,20 @@ if __name__ == "__main__":
 					if train_number < test_number:
 						continue
 
-					for mode in ["PCA", "LDA"]:
-						knn = kNN.knn_init(data = images)
-						knn.run(k=k, train_number = train_number, test_number = test_number, eigenspace_dimension=dim, mode = mode)
+					for mode in mode_list:
+						module = modules.module_init(data = images)
+						module.knn_run(k = k, train_number = train_number, test_number = test_number, eigenspace_dimension=dim, mode = mode)
+	'''
 
+
+	 
+	for dim in eigenspace_list:
+		for train_number in train_number_list:
+			for test_number in test_number_list:
+				if train_number < test_number:
+					continue
+				
+				for mode in mode_list:
+					module = modules.module_init(data =images)
+					module.random_forest_run(train_number = train_number, test_number = test_number, eigenspace_dimension = dim, mode = mode)
 
---
+++ HW05_LDA/modules/modules.py
@@ -0,0 +1,173 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+import time
+
+class module_init:
+
+	'''
+		kNN and Random forest implementation
+
+			written by 21300008 Kang Min-Su
+	'''
+
+
+	def __init__(self, data):
+		self._train_x, self._train_y, self._test_x, self._test_y = data
+		# data init
+
+	def knn_run(self, k, train_number, test_number, eigenspace_dimension, mode):
+
+		'''
+			do kNN
+		'''
+
+		self._data_preprocessor(train_number = train_number, test_number = test_number, eigenspace_dimension = eigenspace_dimension, mode = mode)
+
+		accuracy = 0.0
+		start_time = time.time()
+
+		for test_img_idx in np.ndindex(self._test_x.shape[:1]):
+
+			distance = np.linalg.norm(self._test_x[test_img_idx] - self._train_x, axis=1)	
+			# calculate distances from one test data point to train data samples
+
+			sorted_indices = np.argsort(distance)	
+			# sort index of distances in increasing order
+
+			pred_class = stats.mode(self._train_y[sorted_indices[:k]])[0][0]
+			# get candidate class labels and select the mode value of them
+
+			if self._test_y[test_img_idx] == pred_class:
+				accuracy += 1
+			# calculate acc
+
+		time_spent = time.time() - start_time
+		accuracy /= test_number
+
+		self._result_writer(eigenspace_dimension=eigenspace_dimension, k=k, accuracy=accuracy, time_spent = time_spent, train_number = train_number, test_number = test_number, mode = mode) 
+		# kNN main
+
+
+
+	def random_forest_run(self, train_number, test_number, eigenspace_dimension, mode):
+
+		'''
+			do Random Forest
+		'''
+
+		self._data_preprocessor(train_number = train_number, test_number = test_number, eigenspace_dimension = eigenspace_dimension, mode = mode)
+
+		start_time = time.time()	
+
+		clf = RandomForestClassifier()
+		clf.fit(self._train_x.real, self._train_y)
+
+		acc = np.mean(np.equal(clf.predict(self._test_x.real), self._test_y))
+		time_spent = time.time() - start_time
+
+		self._result_writer(eigenspace_dimension=eigenspace_dimension, k="Random Forest", accuracy=acc, time_spent = time_spent, train_number = train_number, test_number = test_number, mode = mode)
+
+
+
+	def _data_preprocessor(self, train_number, test_number, eigenspace_dimension, mode):
+		'''
+			do PCA or LDA, and split data
+		'''
+		self._train_x = self._train_x[:train_number]
+		self._train_y = self._train_y[:train_number]
+		self._test_x = self._test_x[:test_number]
+		self._test_y = self._test_y[:test_number]
+		# split data 
+
+		if eigenspace_dimension is not None :
+			if mode == "PCA":
+				self._train_x, self._test_x = self.PCA(eigenspace_dimension)
+			elif mode == "LDA":
+				self._train_x, self._test_x = self.LDA(eigenspace_dimension)
+		# do PCA or LDA according to eigenspace dimension
+
+
+	def _result_writer(self, eigenspace_dimension, accuracy, train_number, test_number, time_spent, mode, k = None):
+		'''
+			record learning result 
+		'''
+
+		f = open("./record.txt", "a")
+
+		print("\nTrain number:", train_number, "\t Test number:", test_number)
+		f.write("\nTrain number: " + str(train_number)+ "\t Test number:" + str(test_number)+ "\n")
+
+		if eigenspace_dimension == None:
+			print("\t[Result] k=", k, "\tTotal Acc is ", accuracy, "\tTime:", time_spent, "seconds")
+			f.write("\t[Result] k="+str(k)+"\tTotal Acc is " +str(accuracy)+ "\tTime:"+ str(time_spent)+ "seconds\n") 
+		else:
+			print("\t[Result] mode=", mode,  "\tk=", k, "\teigenspace dimension=", eigenspace_dimension,"\tTotal Acc is ", accuracy, "\tTime:", time_spent, "seconds")
+			f.write("\t[Result] mode="+ str(mode)+ "\tk="+str(k)+"\teigenspace dimension="+str(eigenspace_dimension)+"\tTotal Acc is "+str(accuracy) + "\tTime:" + str(time_spent)+ "seconds\n")
+
+		f.close()
+		# for recoding model performances
+
+
+	def PCA(self, eigenspace_dim):
+
+		'''
+			do PCA analysis
+		'''
+		cov_matrix = np.cov((self._train_x - np.mean(self._train_x, axis = 0)).T)
+		eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+		eigenvectors =eigenvectors.T[:eigenspace_dim].T
+		# do eigen-decomposition
+
+		projectioned_train = np.dot(self._train_x, eigenvectors)
+		projectioned_test = np.dot(self._test_x, eigenvectors)
+		# project data onto n-eigenspace
+ 
+		return (projectioned_train, projectioned_test) 
+		# project data on eigenspace	
+
+
+	def LDA(self, eigenspace_dim):
+		''' 
+			do LDA analysis
+		'''
+
+		class_number = 10
+		class_feature_number = np.array([])
+		variance_within_class = np.zeros(784*784).reshape(-1, 784)
+		variance_between_class = np.zeros(784*784).reshape(-1, 784)
+
+		global_mean = np.mean(self._train_x, axis = 0)
+
+		for i in np.ndindex(class_number):
+			class_features = self._train_x[self._train_y == i]
+			class_mean = np.mean(class_features, axis = 0)
+			# calculate mean of each class
+
+			for j in np.ndindex(class_features.shape[0]):
+				features_minus_class_mean = (class_features[j] - class_mean).reshape(784, 1)
+				variance_within_class += np.dot(features_minus_class_mean, features_minus_class_mean.T)
+				# calculate variance within class to minimize variance between samples in class
+
+			dist_between_class_data = (class_mean - global_mean).reshape((784, 1))
+			variance_between_class += (class_features.shape[0] * np.dot(dist_between_class_data, dist_between_class_data.T))
+			# calculate variance between classes to maximize variance between classes in sample space
+
+		eigenvalues, eigenvectors = np.linalg.eig(np.dot(np.linalg.pinv(variance_within_class), variance_between_class))
+		# eigen-decompose (SW)^(-1) X (SB)
+		eigen_pairs = [(np.abs(eigenvalues[i]), eigenvectors[:,i]) for i in range(len(eigenvalues))]
+		eigen_pairs = sorted(eigen_pairs, key=lambda k: k[0], reverse=True)
+		eigenvectors = np.array([eigen_pairs[i][1] for i in range(eigenspace_dim)]).T
+		# sort (eigenvalue, eigenvector) pair
+
+		projected_train = np.dot(self._train_x, eigenvectors)
+		projected_test = np.dot(self._test_x, eigenvectors)
+		# project train and test samples
+
+		return projected_train, projected_test
+
+
